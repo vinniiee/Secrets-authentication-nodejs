bc7c363907a879a0062f1444897fb73e3ef5b796
vinniiee <vs28245@gmail.com>
2021-04-09 15:30:04 +0530

    upgrade to bycrypt with 10 saltrounds

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-console.log(process.env.API_KEY)
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -30,25 +32,27 @@ const userSchema = new mongoose.Schema ({
   },
 });
 
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
 
 
 const User = new mongoose.model("User",userSchema);
 
 app.post("/register",function(req,res){
-  const newUser = new User(
-    {
-        email: req.body.username,
-        password: req.body.password,
-    });
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }
-      else{
-        res.render("secrets");
+  bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+    const newUser = new User(
+      {
+          email: req.body.username,
+          password: hash,
+      });
+      newUser.save(function(err){
+        if(err){
+          console.log(err);
         }
-    });
+        else{
+          res.render("secrets");
+          }
+      });
+  });
+
 
 });
 
